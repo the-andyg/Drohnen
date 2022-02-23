53a7b735750bb2e5d1b6acf2c1bdd402fda0b2de
Kevin2471 <k_power2406@yahoo.de>
2022-02-23 14:59:39 +0100

    beide Fehlermeldungen zusammen

--- Kommentar.php
+++ Kommentar.php
@@ -5,6 +5,16 @@
     <title>"thema name" kommentieren</title>
     <link rel="stylesheet" type="text/css" href="Style.css">
 </head>
+
+<?php
+$error = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST['textarea'])) {
+        $error = "Der Kommentar darf nicht leer sein!";
+    }
+}
+?>
+
 <body>
 <h1 class="Überschrift center">
     Drohnenforum
@@ -30,8 +40,11 @@
 
     <!-- php einfügen -->
 
-    <form>
-        Text:<br> <textarea rows="6" class="textarea"></textarea><br><br>
+    <form method="post" action="Kommentar.php">
+        Text:<br> <textarea rows="6" class="textarea" name="textarea"></textarea><br><br>
+        <div class="red">
+            <?php echo $error; ?>
+        </div>
         <div class="wrapper">
             <div class="box1"><a href="Hauptseite.php">abbrechen</a></div>
             <div class="box2"><input type="submit" value="absenden" class="absenden"></div>
